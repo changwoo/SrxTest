3609b175f71acc0b71fa063fa68c01ac87c07343
Changwoo Ryu <cwryu@debian.org>
2017-06-11 18:51:52 +0900

    Korean sentence rule as "Generic"

--- src/main/java/com/github/spellcheck_ko/srxtest/App.java
+++ src/main/java/com/github/spellcheck_ko/srxtest/App.java
@@ -12,7 +12,7 @@ public class App
     public static void main( String[] args )
     {
 	String sentence = "언젠 간 가겠지, 푸르른 이 청춘. 지고 또 피는 꽃잎처럼 닭밝은 밤이면 창가에 흐르는 내 젊은 연가가 구슬퍼. 가고없는 날들을 잡으려 잡으려... 빈 손짓에 슬퍼지면 차라리 보내야지 돌아서야지. 그렇게 세월은 가는거야.";
-	String langCode = "en";
+	String langCode = "ko";
 	SrxDocument srxDoc = SrxTools.createSrxDocument("segment.srx");
 	List<String> tokens = SrxTools.tokenize(sentence, srxDoc, langCode);
 
