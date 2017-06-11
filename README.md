6dc2c4e88c27f847b57a1cd54089428771ea3b83
Changwoo Ryu <cwryu@debian.org>
2017-06-11 18:38:28 +0900

    Initial SRX testing base

---
+++ README.md
@@ -0,0 +1,3 @@
+SRX segment test for Languagetool
+
+Under the same license as LanguageTool, MPL/GPL/LGPL.
---
+++ src/main/java/com/github/spellcheck_ko/srxtest/App.java
@@ -0,0 +1,23 @@
+package com.github.spellcheck_ko.srxtest;
+
+import net.loomchild.segment.srx.SrxDocument;
+import java.util.List;
+
+/**
+ * Hello world!
+ *j
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+	String sentence = "언젠 간 가겠지, 푸르른 이 청춘. 지고 또 피는 꽃잎처럼 닭밝은 밤이면 창가에 흐르는 내 젊은 연가가 구슬퍼. 가고없는 날들을 잡으려 잡으려... 빈 손짓에 슬퍼지면 차라리 보내야지 돌아서야지. 그렇게 세월은 가는거야.";
+	String langCode = "en";
+	SrxDocument srxDoc = SrxTools.createSrxDocument("segment.srx");
+	List<String> tokens = SrxTools.tokenize(sentence, srxDoc, langCode);
+
+	for (String s: tokens) {
+	    System.out.println(s);
+	}
+    }
+}
