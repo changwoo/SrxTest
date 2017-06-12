4fb173b14faac0557353ae5d6a9a6edbb0c1d4b3
Changwoo Ryu <cwryu@debian.org>
2017-06-12 21:37:28 +0900

    Add Korean specific SRX rules

--- src/main/java/com/github/spellcheck_ko/srxtest/App.java
+++ src/main/java/com/github/spellcheck_ko/srxtest/App.java
@@ -2,6 +2,9 @@ package com.github.spellcheck_ko.srxtest;
 
 import net.loomchild.segment.srx.SrxDocument;
 import java.util.List;
+import java.util.Scanner;
+import java.lang.StringBuilder;
+import java.io.IOException;
 
 /**
  * Hello world!
@@ -11,13 +14,25 @@ public class App
 {
     public static void main( String[] args )
     {
-	String sentence = "언젠 간 가겠지, 푸르른 이 청춘. 지고 또 피는 꽃잎처럼 닭밝은 밤이면 창가에 흐르는 내 젊은 연가가 구슬퍼. 가고없는 날들을 잡으려 잡으려... 빈 손짓에 슬퍼지면 차라리 보내야지 돌아서야지. 그렇게 세월은 가는거야.";
-	String langCode = "ko";
+	String langCode = args[0];
+	StringBuilder sb = new StringBuilder();
+
+	Scanner scanner = new Scanner(System.in);
+	if (scanner.hasNextLine()) {
+	    sb.append(scanner.nextLine().trim());
+	    while (scanner.hasNextLine()) {
+		sb.append('\n');
+		sb.append(scanner.nextLine().trim());
+	    }
+	}
+
+	String sentence = sb.toString();
+
 	SrxDocument srxDoc = SrxTools.createSrxDocument("segment.srx");
 	List<String> tokens = SrxTools.tokenize(sentence, srxDoc, langCode);
 
-	for (String s: tokens) {
-	    System.out.println(s);
+	for (int i = 0; i < tokens.size(); ++i) {
+	    System.out.println("S[" + i + "]: " + (tokens.get(i)));
 	}
     }
 }
--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-java -jar target/SrxTest-1.0-SNAPSHOT-jar-with-dependencies.jar $*
+#java -jar target/SrxTest-1.0-SNAPSHOT-jar-with-dependencies.jar "$@"
+java -jar target/SrxTest-1.0-SNAPSHOT-jar-with-dependencies.jar ko_two < test.txt
